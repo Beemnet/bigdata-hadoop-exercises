7f8b52459527cc73d4d231c6a12c0206144e6104
Beemnet <bemnetteklu@gmail.com>
2024-04-28 12:18:30 +0000

    bug fixes in ex 7

--- hadoop_bigdata/src/main/java/epita/fr/exercise_7/InvertedIndexMapper.java
+++ hadoop_bigdata/src/main/java/epita/fr/exercise_7/InvertedIndexMapper.java
@@ -9,15 +9,20 @@ public class InvertedIndexMapper extends Mapper<LongWritable, Text, Text, Text> 
     @Override
     public void map(LongWritable key, Text value, Context context)
             throws IOException, InterruptedException {
-        String[] parts = value.toString().split("\t");
-        String[] words = parts[1].split("\\s+");
-        String sentenceId = parts[0];
-        
-        for (String word : words) {
-            // Ignore common words
-            if (!word.equalsIgnoreCase("and") && !word.equalsIgnoreCase("or") && !word.equalsIgnoreCase("not")) {
-                context.write(new Text(word.toLowerCase()), new Text(sentenceId));
+        String[] parts = value.toString().trim().split("  "); // Split based on two spaces
+        if (parts.length >= 2) {
+            String[] words = parts[1].split("\\s+");
+            String sentenceId = parts[0];
+
+            for (String word : words) {
+                // Ignore common words
+                if (!word.equalsIgnoreCase("and") && !word.equalsIgnoreCase("or") && !word.equalsIgnoreCase("not")) {
+                    context.write(new Text(word.toLowerCase()), new Text(sentenceId));
+                }
             }
+        } else {
+            // Handle malformed input
+            System.err.println("Malformed input: " + value.toString());
         }
     }
 }
